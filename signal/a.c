5eda1d5e1e04023cb5d76e5f031bb8496325daaa
Gopher <gopher0727@gmail.com>
2026-04-18 00:57:58 +0800

    add signal

---
+++ signal/a.c
@@ -0,0 +1,29 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handler_siguser1(int sig) {
+	printf("\nReceived SIGUSER1 signal! Continuing...\n");
+}
+
+int main() {
+	// 现在，当程序 B 发出 SIGUSER1 信号，程序 A 执行此处理函数而不会默认中断
+	signal(SIGUSR1, handler_siguser1);
+
+	printf("Process ID: %d\n", getpid());
+
+	const char* message = "Subscribe";
+	while (1) {
+		printf("\r           ");
+		printf("\r%s", message);
+		fflush(stdout);
+		sleep(1);
+		for (int i = 0; i < 3; i++) {
+			printf(".");
+			fflush(stdout);
+			sleep(1);
+		}
+	}
+
+	return 0;
+}
---
+++ signal/b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	pid_t pid = atoi(argv[1]);
+
+	kill(pid, SIGUSR1);
+
+	printf("Sent signal to process %d\n", pid);
+
+	return 0;
+}
